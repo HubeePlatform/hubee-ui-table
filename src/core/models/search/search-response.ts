dd0d108f512227803036e6015415de2ed0c02214
Miranda <luiz.assis.ti@gmail.com>
2023-01-19 11:54:45 -0300

    Add new property of search model in response

--- src/core/models/search/search-response.ts
+++ src/core/models/search/search-response.ts
@@ -1,18 +1,21 @@
-import { PaginationModel } from '@/core/models';
+import { PaginationModel, SearchModel } from '@/core/models';
 
 export default class SearchResponseModel<T> {
+    searchModel: SearchModel;
     page: PaginationModel;
     data: T;
 
-    constructor(data: T, page: PaginationModel) {
+    constructor(data: T, page: PaginationModel, searchModel: SearchModel) {
         this.data = data;
         this.page = page;
+        this.searchModel = searchModel;
     }
 
     static makeEmpty(): SearchResponseModel<any> {
         return new SearchResponseModel<any>(
             [] as any[],
             new PaginationModel({}),
+            new SearchModel('', '', []),
         );
     }
 }
